c191ea6afcf457f5cba363807733ecf832446271
machida0902 <machida@tk2-401-41738.vs.sakura.ne.jp>
2017-12-25 21:18:22 +0900

    test commit

--- main.go
+++ main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World")
-}
-
 func main() {
-	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
-	http.ListenAndServe(":8082", nil)
+	fmt.Println("hello world")
 }
---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+go run /home/machida/devgo/src/first/main.go
